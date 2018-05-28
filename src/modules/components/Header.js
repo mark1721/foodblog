12f20fe6acb7dfc5089ce37da4efa04ce009b502
mark1721 <arc.ranges@live.ca>
2018-05-28 16:11:31 -0700

    added header

--- src/modules/components/Header.js
+++ src/modules/components/Header.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flex: "100%"
+  },
+  buttons: {
+    flex: 1
+  }
+}
 class NavBar extends Component {
   render() {
     return (
-      <AppBar color="primary" position="static">
+      <AppBar style={styles.root} color="primary" position="static">
         <Toolbar>
-          <Typography variant="title" color="inherit">
+          <Typography style={styles.flex} variant="title" color="inherit">
             Food.com
           </Typography>
+          <Button style={styles.buttons} color="inherit">Home</Button>
+          <Button style={styles.buttons} color="inherit">About</Button>
+          <Button style={styles.buttons} color="inherit">Blog</Button>
+          <Button style={styles.buttons} color="inherit">Projects</Button>
+          <Button style={styles.buttons} color="inherit">Contact Us</Button>
         </Toolbar>
       </AppBar>
     );
